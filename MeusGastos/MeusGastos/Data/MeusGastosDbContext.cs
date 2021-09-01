482abb0f4d7f83233c80b9206aa77fa2a250d66d
DaniloSTFR <danilo.santosfr@gmail.com>
2021-09-01 01:24:45 -0300

    Projeto Ciado. Identidade configurada. Login ativo

---
+++ MeusGastos/MeusGastos/Data/MeusGastosDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MeusGastos.Data
+{
+    public class MeusGastosDbContext : IdentityDbContext
+    {
+        //public MeusGastosDbContext(DbContextOptions<MeusGastosDbContext> options)
+        //    : base(options)
+        //{
+        //}
+
+        private readonly DbContextOptions _options;
+
+        public MeusGastosDbContext(DbContextOptions options) : base(options)
+        {
+            _options = options;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
